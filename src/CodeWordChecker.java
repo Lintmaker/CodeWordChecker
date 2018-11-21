cbf073922366da7a5cc7cf1d38e1d3cd53b02165
BT_1N3_32@bktech.local <BT_1N3_32@bktech.local>
2018-11-21 14:16:07 -0500

    I named the one I made before this wrong and that is why there is one commit.

---
+++ src/CodeWordChecker.java
@@ -0,0 +1,28 @@
+public class CodeWordChecker implements StringChecker {
+    private String nope;
+    private int start;
+    private int end;
+
+    public CodeWordChecker(int minLen, int maxLen, String symbol)
+    {
+        this.nope = symbol;
+        this.start = minLen;
+        this.end = maxLen;
+    }
+    public CodeWordChecker(String symbol)
+    {
+        this.start = 6;
+        this.end = 20;
+        this.nope = symbol;
+    }
+
+    @Override
+    public boolean isValid(String str)
+    {
+        if (str.indexOf(this.nope) == -1 && str.length() >= this.start && str.length() <= this.end)
+        {
+            return true;
+        }
+        return false;
+    }
+}
